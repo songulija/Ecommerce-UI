4dcbad90ead5fbb9794fac5815c628abddafb17a
songulija <65647664+songulija@users.noreply.github.com>
2022-01-18 09:30:53 +0200

    Changed design of HomeScreen top part

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -9,6 +9,10 @@ import Message from '../components/Message.js';
 // import ProductCarousel from '../components/ProductCarousel.js';
 import Meta from '../components/Meta.js';
 import '../styles/home.css'
+import women1 from '../images/home/women-1.jpg'
+import women2 from '../images/home/women-2.jpg'
+import males from '../images/home/males.jpg'
+import acessoriesImg from '../images/home/acessories.jpg'
 
 function HomeScreen({ match }) {
     //Getting keyword(param) from url. /search/:keyword. we go to HomeScreen so we can get it
@@ -23,9 +27,29 @@ function HomeScreen({ match }) {
     return (
         <>
             {/* <!-- banner start --> */}
-            <section class="banner-section">
+            {/* <section className="banner-section"> */}
+            <div className='photos-holder'>
+                <img src={women1} alt="Pagrindinė nuotrauka" />
+                <div className='categories-container'>
+                    {/* <img src={males} alt='Vyru kategorija' />
+                    <img src={women2} alt='Moterų kategorija' /> */}
+                    <div className="top-categories">
+                        <div className='males'>
+                            <img src={males} alt='Vyru kategorija' />
+                        </div>
+                        <div className='females'>
+                            <img src={women2} alt='Moterų kategorija' />
+                        </div>
+                    </div>
+                    <div className='accesories-categories'>
+                        <img src={acessoriesImg} alt="Aksesuarai"/>
+                    </div>
 
-            </section>
+
+                </div>
+
+            </div>
+            {/* </section> */}
             {/* <!-- banner end --> */}
             <Meta />
             {/* {!keyword ? <ProductCarousel /> : <Link to='/' className='btn btn-light'>Go Back </Link>} */}
